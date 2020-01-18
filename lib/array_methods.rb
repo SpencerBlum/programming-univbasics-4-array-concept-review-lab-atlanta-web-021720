776fe445dc15e01164aa7e89d5e26f5275e64f5d
Spencer Blum <spencerblum@gmail.com>
2020-01-18 18:44:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   i = 0
   while array[i] < array.length do
    if array[i] === value_to_find
-     return array[i]
+     return [i]
    else
     i += 1
   end
